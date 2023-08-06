406512fad6b430498b7bdfae3efc8f58fc3585c4
Peter Kimberley <p3t3rk1m0@gmail.com>
2023-08-06 15:23:19 +0100

    -Added interactive loop.
    Signed-off-by: Peter Kimberley <p3t3rk1m0@gmail.com>

--- python_flash_card/flash_card_python.py
+++ python_flash_card/flash_card_python.py
@@ -9,13 +9,30 @@ glossary = {'word1': "definition1",
             'word2':"definition2", 
             'word3': "definition3"}
 
+# sets the flag variable to False
+exit = False
+
 # the flashcard function
 def flashcard():
     """Show a falsh card to the user
     and ask for a definition"""
     random_key = choice(list(glossary))
     print("define", random_key)
+    input("Press return to see the definition.")
+    print(glossary[random_key])
     
+
+while not exit:
+    user_input = input("Press s to show flashcard or Q to quit.")
+    if user_input == 'q':
+         exit = True
+    elif user_input == 's':
+        flashcard()
+    else:
+        print("You need to enter either q, or s.")
+            
+        
+       
 
 
 
