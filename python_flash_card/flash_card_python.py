22a7f6f5c9cc62a17192c42a7f792db676b0b361
Peter Kimberley <p3t3rk1m0@gmail.com>
2023-08-06 16:15:57 +0100

    -The finished program  This program now contains the  TM112 flashcard system built using  the csv file as a dictionary
    Signed-off-by: Peter Kimberley <p3t3rk1m0@gmail.com>

--- python_flash_card/flash_card_python.py
+++ python_flash_card/flash_card_python.py
@@ -1,16 +1,10 @@
-# flash_card_python
-# a flash card systme for python
+# flash_cards_for_Tm112_gloassary
+# a flash card system in python
 
-
+#Importing built in fuctions
 from random import *
+import csv
 
-# setting up the glossary
-glossary = {'word1': "definition1", 
-            'word2':"definition2", 
-            'word3': "definition3"}
-
-# sets the flag variable to False
-exit = False
 
 # the flashcard function
 def flashcard():
@@ -20,10 +14,28 @@ def flashcard():
     print("define", random_key)
     input("Press return to see the definition.")
     print(glossary[random_key])
-    
 
+# function to import file to dictionary
+def file_to_dictionary(filename):
+    """This will be using a file
+        and importing it to a dictionary
+        to use with flash cards"""
+    file = open(filename, 'r')
+    reader = csv.reader(file)
+    dictionary = {}
+    for row in reader:
+        dictionary[row[0]] = row[1]
+    return dictionary
+
+# setting up the glossary using an imported CSV file
+glossary = file_to_dictionary("TM112_Glossary.txt")
+
+# sets the flag variable to False
+exit = False    
+
+# running the interactive loop
 while not exit:
-    user_input = input("Press s to show flashcard or Q to quit.")
+    user_input = input("Press s to show flashcard or q to quit.")
     if user_input == 'q':
          exit = True
     elif user_input == 's':
